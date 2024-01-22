86845543e849081a86d44e88fd1371e2bdafdde6
Chris Luna <raffagapro@gmail.com>
2024-01-22 17:46:11 -0600

    new master

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import Card from './components/Card/Card.jsx';
 import Cards from './components/Cards/Cards.jsx';
 import SearchBar from './components/SearchBar/SearchBar.jsx';
+
+//emula la base de datos
 import characters, { Rick } from './data.js';
 
 function App() {
+
+   const onClose = (id) => window.alert(`Emulamos que se cierra la card id ${id}`);
+
    return (
       <div className='App'>
          <SearchBar onSearch={(characterID) => window.alert(characterID)} />
-         <Cards 
-            characters={characters}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
-         />
+         <Cards characters={characters} onClose={onClose} />
          <Card
             id={Rick.id}
             name={Rick.name}
@@ -20,7 +22,7 @@ function App() {
             gender={Rick.gender}
             origin={Rick.origin.name}
             image={Rick.image}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
+            onClose={onClose}
          />
       </div>
    );
